7f7bb9bdc68d51593b8a9ef9dc3f54e7ab31d66d
Benjamin Christau <benjamin.christau@luos.io>
2022-07-04 18:27:45 +0200

    Change destruct usage of process.env variable in packages

--- packages/services/mongo/mongodbAdapter.ts
+++ packages/services/mongo/mongodbAdapter.ts
@@ -2,9 +2,7 @@ import { MongoClient, MongoClientOptions, ServerApiVersion } from 'mongodb';
 
 import { decryptedMongoDBCert, decryptedMongoDBKey } from './cert';
 
-const { VERCEL_ENV, MONGODB_URI } = process.env;
-
-if (MONGODB_URI === undefined) {
+if (process.env.MONGODB_URI === undefined) {
   throw new Error('Missing MONGODB_URI environment variable');
 }
 
@@ -18,12 +16,12 @@ const options: MongoClientOptions = {
   serverApi: ServerApiVersion.v1,
 };
 
-if (VERCEL_ENV === 'development') {
+if (process.env.VERCEL_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   // @ts-ignore
   if (!global._mongoClientPromise) {
-    client = new MongoClient(MONGODB_URI, options);
+    client = new MongoClient(process.env.MONGODB_URI, options);
     // @ts-ignore
     global._mongoClientPromise = client.connect();
   }
@@ -31,11 +29,11 @@ if (VERCEL_ENV === 'development') {
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(MONGODB_URI, options);
+  client = new MongoClient(process.env.MONGODB_URI, options);
   clientPromise = client.connect();
 }
 
-export const mongoClient = new MongoClient(MONGODB_URI, options);
+export const mongoClient = new MongoClient(process.env.MONGODB_URI, options);
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.

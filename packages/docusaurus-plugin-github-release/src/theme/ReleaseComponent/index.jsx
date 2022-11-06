e7c65d4646d5b60e642871d86f62be91c551d8ea
Benjamin Christau <benjamin.christau@luos.io>
2022-11-06 18:58:00 +0100

    [Packages/docusaurus-plugin-github-releases] Update ReleaseComponent to adapte to current URL location to load release content

--- packages/docusaurus-plugin-github-release/src/theme/ReleaseComponent/index.jsx
+++ packages/docusaurus-plugin-github-release/src/theme/ReleaseComponent/index.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useVersions } from '@docusaurus/plugin-content-docs/client';
+import { useLocation } from '@docusaurus/router';
 import { useDocsPreferredVersion } from '@docusaurus/theme-common';
-import useRouteContext from '@docusaurus/';
 import { major, minor } from 'semver';
 
 export default function ReleaseComponent() {
   const { preferredVersion } = useDocsPreferredVersion();
-  const { plugin, data } = useRouteContext();
+  const allVersions = useVersions();
+  const location = useLocation();
   const [releases, setReleases] = useState([]);
 
   console.log('ReleaseComponent, routectx', plugin, data);
@@ -15,6 +17,18 @@ export default function ReleaseComponent() {
       import(
         `@github/release-${major(preferredVersion.label)}.${minor(preferredVersion.label)}.x.json`
       ).then(({ default: releases }) => setReleases(releases));
+    } else {
+      const foundCurrentVersion = allVersions.find(
+        (version) => version.name !== 'current' && location.pathname.indexOf(version.path) !== -1,
+      );
+      console.log('ReleaseComponent, foundCurrentVersion', foundCurrentVersion);
+      if (foundCurrentVersion) {
+        import(
+          `@github/release-${major(foundCurrentVersion.label)}.${minor(
+            foundCurrentVersion.label,
+          )}.x.json`
+        ).then(({ default: releases }) => setReleases(releases));
+      }
     }
   }, [preferredVersion]);
 

319d59345394943c21a286063f4084bc0fbd950e
Benjamin Christau <benjamin.christau@luos.io>
2022-12-08 14:22:34 +0100

    Try to fix things with environment variables #2

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,10 +1,10 @@
-import type { NextPage } from 'next';
-
 import Introduction from 'components/introduction';
 import Integration from 'components/integration';
 import Software from 'components/software';
 import Anatomy from 'components/anatomy';
 import Powered from 'components/powered';
+
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
